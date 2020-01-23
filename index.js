92047292350e1c573e5b92f65b85e761bdf3ce97
Glen Keane <glenkeane.94@gmail.com>
2020-01-23 15:38:24 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const core = require('@actions/core');
+const { GitHub } = require('@actions/github');
+
+const path = require('path');
+const fs = require('fs');
+const fsPromises = fs.promises;
+
+async function run() {
+  try {
+    // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
+    const github = new GitHub(process.env.GITHUB_TOKEN);
+
+    // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
+    const uploadUrl = core.getInput('upload_url', { required: true });
+    const assetsPath = core.getInput('assets_path', { required: true });
+
+		await fsPromises.access(assetsPath, fs.constnts.R_OK)
+
+		const filenames = await fsPromises.readdir(assetsPath)
+
+		for (let filename of filenames) {
+			const fullpath = path.join(__dirname, assetsPath, filename)
+			const isExe = fullpath.endsWith('.exe')
+
+      // Determine content-length for header of upload asset
+      const contentLength = (await fsPromises.stat(fullpath)).size
+
+      // Setup headers for API call, see Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset for more information
+      const headers = {
+				'content-type': isExe ? 'application/vnd.microsoft.portable-executable' : 'application/octet-stream',
+				'content-length': contentLength
+			};
+
+			const fileContent = await fsPromises.readFile(fullpath)
+
+      // Upload a release asset
+      // API Documentation: https://developer.github.com/v3/repos/releases/#upload-a-release-asset
+      // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset
+      await github.repos.uploadReleaseAsset({
+      	headers,
+      	url: uploadUrl,
+      	name: filename,
+      	file: fileContent
+      });
+		}
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
